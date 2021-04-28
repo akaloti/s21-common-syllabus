cc0a64c3a73669e144713094a88dffff47584a6e
Aaron Kaloti <aarons.7007@gmail.com>
2021-04-27 23:42:40 -0700

    Init

---
+++ include_all.mk
@@ -0,0 +1,3 @@
+# Include this makefile in the makefile that creates the syllabus.
+# TODO: Can probably use a wildcare here.
+syllabus.pdf: ../../common_syllabus/cheating.tex ../../common_syllabus/oh_layout.tex ../../common_syllabus/emailing.tex ../../common_syllabus/grading_cutoffs.tex ../../common_syllabus/communication_intro.tex
